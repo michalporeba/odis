c3cd23c0db4db65c04c12b7e58599b9e5322d1a8
Michał Poręba <michalporeba@gmail.com>
2022-01-07 21:24:11 +0000

    improving the registry-dispatch technique

--- code/scratchpad/plugins/registry_dispatch.py
+++ code/scratchpad/plugins/registry_dispatch.py
@@ -23,5 +23,41 @@ for p in PluginBase.hello():
     print(p)
 
 
+print('----------------------SECOND EXPERIMENT--------')
+
+class FancyPluginBase:
+    instances = []
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.instances.append(cls())
+
+    @staticmethod
+    def accept(value: any) -> list:
+        if type(value) == list:  
+            return value
+        else: 
+            return [value]
+
+    def dispatch(type: any = None) -> list: 
+        return [r for i in FancyPluginBase.instances for r in FancyPluginBase.accept(i.respond_to(type))]
+
+    def respond_to(self, type: any = None) -> any:
+        return []
+    
 
 
+class First(FancyPluginBase):
+    def respond_to(self, type: any = None) -> any:
+        return 'from first'
+
+class Second(FancyPluginBase):
+    def respond_to(self, type: any = None) -> any:
+        return ['from second']
+
+class Third(FancyPluginBase):
+    pass
+
+
+for p in FancyPluginBase.dispatch():
+    print(p)
