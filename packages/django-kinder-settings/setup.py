c69f3f952ac95a248af7d21f9ed2358e7bd23930
Michał Poręba <michalporeba@gmail.com>
2022-01-07 16:16:08 +0000

    preparing django-kinder-settings for future publication

---
+++ packages/django-kinder-settings/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+def long_description():
+    with open('README.md', 'r') as readme:
+        return readme.read()
+
+setup(
+    name='django-kinder-settings',
+    packages=find_packages(include=['django_kinder_settings']),
+    version='0.0.8',
+    author='Michal Poreba',
+    license='MIT',
+    description='A kinder sort of django settings',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalporeba/toposearch/packages/django-kinder-settings',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django :: 4.0',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development'
+    ],
+    install_requires=['django'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests'
+)
