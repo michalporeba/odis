16e0f71883f9dc91a809a8402a712dac1b4af697
Michał Poręba <michalporeba@gmail.com>
2022-01-07 22:48:59 +0000

    working example of better dispatcher

--- code/scratchpad/plugins/better_registry_dispatch.py
+++ code/scratchpad/plugins/better_registry_dispatch.py
@@ -1,9 +1,29 @@
+from typing import Callable
+
 class PluginBase:
     instances = []
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls.instances.append(cls())
+
+    def query(func: Callable) -> Callable:
+        def wrapper():            
+            results = []
+            for i in PluginBase.instances:
+                fn = func.__name__
+                if getattr(i, fn, None) \
+                    and not getattr(PluginBase, fn).__qualname__==getattr(i, fn).__qualname__:
+                    try:
+                        results += PluginBase.accept(getattr(i, fn)())
+                    except Exception as err:
+                        print(err)
+                        pass
+
+            return results
+
+        return wrapper
+
 
     @staticmethod
     def accept(value: any) -> list:
@@ -24,19 +44,31 @@ class PluginBase:
 
         return results
 
-    
+    @query 
+    def sample() -> list: 
+        return []
+
 
 
 class First(PluginBase):
     def dispatch(self) -> any:
         return 'from first'
+    
+    def sample(self) -> any: 
+        return 'sample one'
 
 class Second(PluginBase):
     def dispatch(self) -> any:
         return ['from second']
 
+    def sample(self) -> any: 
+        return 'sample two'
+
 class Third(PluginBase):
     pass
 
-for p in PluginBase.dispatch():
+    def sample(self) -> any:
+        return 'sample three'
+
+for p in PluginBase.sample():
     print(p)
