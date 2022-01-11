1e618ab2ff910dd022bd2df2a3dc15429cbd6064
Michał Poręba <michalporeba@gmail.com>
2022-01-11 22:51:20 +0000

    publishing callouts 0.1.1

--- packages/callouts/setup.py
+++ packages/callouts/setup.py
@@ -7,7 +7,7 @@ def long_description():
 setup(
     name='callouts',
     packages=find_packages(include=['callouts']),
-    version='0.1.0',
+    version='0.1.1',
     author='Michal Poreba',
     license='MIT',
     description='A simple, class based dispatcher',
